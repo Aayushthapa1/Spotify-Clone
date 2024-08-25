f04af662c0b597d7896daa7e93540bf5f76e532a
Aayushthapa1 <aayush134056@gmail.com>
2024-08-25 11:46:37 +0545

    sidebar and musicplayer completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Sidebar from './components/Sidebar'
+import Player from './components/Player'
 
 const App = () => {
   return (
     <div className='h-screen bg-black' >
-      <div className='flex h-[90%]'></div>
+      <div className='flex h-[90%]'>
       <Sidebar />
-      
-      
+      </div>
+      <Player/>
     </div>
+
   )
 }
 
