41cf1aa17603faf6c4e7ddf9c191a65a324d6650
Aayushthapa1 <aayush134056@gmail.com>
2024-08-29 22:36:24 +0545

    half  completed

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from "react";
+import Navbar from "./Navbar";
+import { albumsData } from "../assets/assets";
 
 const DisplayHome = () => {
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex overflow-auto">
+        </div>
+        {albumsData.map((item, index) => (
+          <AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>
+        ))}
+        
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default DisplayHome
+export default DisplayHome;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,8 +26,8 @@ const Navbar = () => {
       </div>
       <div className="flex items-center gap-2 mt-4">
         <p className="bg-white text-black px-4 py-1 rounded-2xl">All</p>
-        <p className="bg-black  px-4 py-1 rounded-2xl">Music</p>
-        <p className="bg-black  px-4 py-1 rounded-2xl">Podcasts</p>
+        <p className="bg-black  px-4 py-1 rounded-2xl cursor-pointer">Music</p>
+        <p className="bg-black  px-4 py-1 rounded-2xl cursor-pointer">Podcasts</p>
         
         
 
